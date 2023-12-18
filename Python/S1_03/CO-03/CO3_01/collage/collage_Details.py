1df5b29bc45820427088f0db87d3f319f43b2d0e
ghsalah <99658661+ghsalah@users.noreply.github.com>
2023-12-18 18:32:06 +0530

    Add files via upload

---
+++ Python/S1_03/CO-03/CO3_01/collage/collage_Details.py
@@ -0,0 +1,3 @@
+def collageDetails():
+    print("Awh Engineering College")
+    print("\n AWH Engineering College (AWHEC), established in 2001\n is a self-financing engineering college managed by Association for Welfare of Handicapped (AWH)\n Kozhikode, Kerala.\n")
